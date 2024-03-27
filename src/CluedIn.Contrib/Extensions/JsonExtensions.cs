3571aaf61d9fb4a2d2eb0576b38374d1accec29a
romaklimenko <romaklimenko@gmail.com>
2024-03-27 20:28:15 +0100

    Method documentation

--- src/CluedIn.Contrib/Extensions/JsonExtensions.cs
+++ src/CluedIn.Contrib/Extensions/JsonExtensions.cs
@@ -4,6 +4,16 @@ namespace CluedIn.Contrib.Extensions;
 
 public static class JsonExtensions
 {
+    /// <summary>
+    ///     Converts a JsonDocument into a Dictionary&gt;string, string&lt;,
+    ///     where each key represents a property path,
+    ///     and value is a stingified JSON object value.
+    ///     So a JSON object { "a": { "b": 1 } } is converted to { "a.b", "1" }.
+    ///     Arrays are flattened with indexed: { "a": [ { "b": "b0" }, { "b": "b1" } ] } ->
+    ///     { { "a.0.b", "b0" }, { "a.1.b", "b1" } }
+    /// </summary>
+    /// <param name="jsonDocument"></param>
+    /// <returns></returns>
     public static Dictionary<string, string> Flatten(this JsonDocument jsonDocument)
     {
         var result = new Dictionary<string, string>();
