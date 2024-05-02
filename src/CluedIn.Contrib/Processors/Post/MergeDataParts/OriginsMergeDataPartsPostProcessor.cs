bdc984c0770f3734fefdee5207323377d15f86f1
Roman Klimenko <rok@cluedin.com>
2024-05-02 17:26:11 +0200

    OriginsMergeDataPartsPostProcessor

---
+++ src/CluedIn.Contrib/Processors/Post/MergeDataParts/OriginsMergeDataPartsPostProcessor.cs
@@ -0,0 +1,28 @@
+﻿using CluedIn.Core.Data.Parts;
+using CluedIn.Core.Processing;
+using CluedIn.Processing;
+using ExecutionContext = CluedIn.Core.ExecutionContext;
+
+namespace CluedIn.Contrib.Processors.Post.MergeDataParts;
+
+public class OriginsMergeDataPartsPostProcessor : IMergeDataPartsPostProcessor
+{
+    public bool IsEnabled(ExecutionContext context)
+    {
+        return true;
+    }
+
+    public void Process(ProcessingContext context, IProcessedEntityMetadataPart entityProcessedData,
+        IEnumerable<IDataPart> orderedDataParts)
+    {
+        entityProcessedData.Properties["system.origins"] = string.Join(
+            ",",
+            entityProcessedData.Codes
+                .Where(x => !x.Origin.ToString().Contains("CluedIn", StringComparison.InvariantCultureIgnoreCase))
+                .Select(x => x.Origin)
+                .Distinct()
+                .OrderBy(x => x));
+    }
+
+    public int Priority => 1;
+}
---
+++ tests/CluedIn.Contrib.Tests/Processors/Post/MergeDataParts/OriginsMergeDataPartsPostProcessorTest.cs
@@ -0,0 +1,102 @@
+﻿using CluedIn.Contrib.Processors.Post.MergeDataParts;
+using CluedIn.Core.Data;
+using CluedIn.Core.Data.Parts;
+using CluedIn.Core.Processing;
+using NSubstitute;
+using ExecutionContext = CluedIn.Core.ExecutionContext;
+
+namespace CluedIn.Contrib.Tests.Processors.Post.MergeDataParts;
+
+public class OriginsMergeDataPartsPostProcessorTest
+{
+    private readonly ExecutionContext _executionContext = Substitute.For<ExecutionContext>();
+    private readonly ProcessingContext _processingContext = Substitute.For<ProcessingContext>();
+
+    [Fact]
+    public void IsEnabled_True()
+    {
+        // Arrange
+        var processor = new OriginsMergeDataPartsPostProcessor();
+        // Act
+        // Assert
+        Assert.True(processor.IsEnabled(_executionContext));
+    }
+
+    [Fact]
+    public void Priority_One()
+    {
+        // Arrange
+        var processor = new OriginsMergeDataPartsPostProcessor();
+        // Act
+        // Assert
+        Assert.Equal(1, processor.Priority);
+    }
+
+    [Fact]
+    public void NoCodes()
+    {
+        // Arrange
+        var processor = new OriginsMergeDataPartsPostProcessor();
+        IProcessedEntityMetadataPart entityProcessedData = new ProcessedEntityMetadataPart();
+        // Act
+        processor.Process(_processingContext, entityProcessedData, Enumerable.Empty<IDataPart>());
+        // Assert
+        Assert.False(entityProcessedData.Properties.ContainsKey("system.origins"));
+    }
+
+    [Fact]
+    public void OneCodeOneOrigin()
+    {
+        // Arrange
+        var processor = new OriginsMergeDataPartsPostProcessor();
+        IProcessedEntityMetadataPart entityProcessedData = new ProcessedEntityMetadataPart();
+        entityProcessedData.Codes.Add(new EntityCode("/Person", "Salesforce", 1));
+        // Act
+        processor.Process(_processingContext, entityProcessedData, Enumerable.Empty<IDataPart>());
+        // Assert
+        Assert.Equal("Salesforce", entityProcessedData.Properties["system.origins"]);
+    }
+
+    [Fact]
+    public void TwoCodesOneOrigin()
+    {
+        // Arrange
+        var processor = new OriginsMergeDataPartsPostProcessor();
+        IProcessedEntityMetadataPart entityProcessedData = new ProcessedEntityMetadataPart();
+        entityProcessedData.Codes.Add(new EntityCode("/Person", "Salesforce", 1));
+        entityProcessedData.Codes.Add(new EntityCode("/Person", "Salesforce", 2));
+        // Act
+        processor.Process(_processingContext, entityProcessedData, Enumerable.Empty<IDataPart>());
+        // Assert
+        Assert.Equal("Salesforce", entityProcessedData.Properties["system.origins"]);
+    }
+
+    [Fact]
+    public void TwoCodesTwoOrigins()
+    {
+        // Arrange
+        var processor = new OriginsMergeDataPartsPostProcessor();
+        IProcessedEntityMetadataPart entityProcessedData = new ProcessedEntityMetadataPart();
+        entityProcessedData.Codes.Add(new EntityCode("/Person", "Salesforce", 1));
+        entityProcessedData.Codes.Add(new EntityCode("/Person", "Dynamics365", 2));
+        // Act
+        processor.Process(_processingContext, entityProcessedData, Enumerable.Empty<IDataPart>());
+        // Assert
+        Assert.Equal("Dynamics365,Salesforce", entityProcessedData.Properties["system.origins"]);
+    }
+
+    [Fact]
+    public void ThreeCodesTwoOriginsOneCluedIn()
+    {
+        // Arrange
+        var processor = new OriginsMergeDataPartsPostProcessor();
+        IProcessedEntityMetadataPart entityProcessedData = new ProcessedEntityMetadataPart();
+        entityProcessedData.Codes.Add(new EntityCode("/Person", "Salesforce", 1));
+        entityProcessedData.Codes.Add(new EntityCode("/Person", "Dynamics365", 2));
+        entityProcessedData.Codes.Add(new EntityCode("/Person", "CluedIn(name)", "Bob"));
+        // Act
+        processor.Process(_processingContext, entityProcessedData, Enumerable.Empty<IDataPart>());
+        // Assert
+        Assert.Equal("Dynamics365,Salesforce", entityProcessedData.Properties["system.origins"]);
+    }
+}
