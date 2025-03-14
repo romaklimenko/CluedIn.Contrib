f512e69e827a50f7d4ff615836b8cc6f61b20c04
Roman Klimenko <rok@cluedin.com>
2025-03-14 16:27:33 +0100

    SumRuleAction

---
+++ src/CluedIn.Contrib/Rules/Actions/SumRuleAction.cs
@@ -0,0 +1,78 @@
+﻿using System.Globalization;
+using CluedIn.Core.Data.Parts;
+using CluedIn.Core.Processing;
+using CluedIn.Core.Rules;
+using CluedIn.Core.Rules.Models;
+using CluedIn.Rules.Actions;
+
+namespace CluedIn.Contrib.Rules.Actions;
+
+public class SumRuleAction : BaseRuleAction, IScopedRuleAction
+{
+    private string SourceVocabularyPrefixWithDot =>
+        string.IsNullOrWhiteSpace(SourceVocabularyPrefixFieldValue)
+            ? string.Empty
+            : SourceVocabularyPrefixFieldValue + ".";
+
+    [RuleProperty(
+        FriendlyName = "Source Vocabulary Prefix",
+        Kind = ActionControlKind.Text,
+        IsRequired = true)]
+    // ReSharper disable once PropertyCanBeMadeInitOnly.Global
+    public string? SourceVocabularyPrefixFieldValue { get; set; }
+
+    [RuleProperty(
+        FriendlyName = "Target Vocabulary Key",
+        Kind = ActionControlKind.Text,
+        IsRequired = true)]
+    // ReSharper disable once PropertyCanBeMadeInitOnly.Global
+    public string? TargetVocabularyKeyFieldValue { get; set; }
+
+    public override string Name => "Sum";
+    public override bool SupportsPreview => true;
+
+    public IEnumerable<ProcessingRuleScope> SupportsProcessingRuleScopes =>
+        new[] { ProcessingRuleScope.DataPart, ProcessingRuleScope.Entity };
+
+    public override RuleActionResult Run(ProcessingContext context, IEntityMetadataPart entityMetadataPart,
+        bool isPreview)
+    {
+        if (string.IsNullOrWhiteSpace(SourceVocabularyPrefixFieldValue) ||
+            string.IsNullOrWhiteSpace(TargetVocabularyKeyFieldValue))
+        {
+            return RuleActionResult.Failure("Source Vocabulary Prefix is required");
+        }
+
+        var sum = 0.0;
+
+        if (isPreview)
+        {
+            entityMetadataPart.Properties[TargetVocabularyKeyFieldValue] =
+                (new Random().NextDouble() * 1000).ToString("F2", CultureInfo.InvariantCulture);
+            return RuleActionResult.Success("OK");
+        }
+
+        foreach (var property in entityMetadataPart.Properties)
+        {
+            if (!property.Key.StartsWith(SourceVocabularyPrefixWithDot))
+            {
+                continue;
+            }
+
+            var keyWithoutPrefix = property.Key[SourceVocabularyPrefixWithDot.Length..];
+            if (keyWithoutPrefix.Contains('.') || keyWithoutPrefix.Contains('+'))
+            {
+                continue;
+            }
+
+            if (double.TryParse(property.Value, NumberStyles.Any, CultureInfo.InvariantCulture, out var value))
+            {
+                sum += value;
+            }
+        }
+
+        entityMetadataPart.Properties[TargetVocabularyKeyFieldValue] = sum.ToString("F2", CultureInfo.InvariantCulture);
+
+        return RuleActionResult.Success("OK");
+    }
+}
---
+++ tests/CluedIn.Contrib.Tests/Rules/Actions/SumRuleActionTest.cs
@@ -0,0 +1,64 @@
+﻿using CluedIn.Contrib.Rules.Actions;
+using CluedIn.Core.Data.Parts;
+using CluedIn.Core.Processing;
+using NSubstitute;
+
+namespace CluedIn.Contrib.Tests.Rules.Actions;
+
+public class SumRuleActionTest
+{
+    // private readonly string _apiKey = Guid.NewGuid().ToString();
+    private readonly ProcessingContext _context = Substitute.For<ProcessingContext>();
+
+    private static IEntityMetadataPart GetEntityMetadataPart()
+    {
+        return new EntityMetadataPart
+        {
+            Properties =
+            {
+                ["test.amount.int"] = "1",
+                ["test.amount.double"] = "3.14",
+                ["test.amount.wat"] = "wat",
+                ["test.amount.nest.int"] = "100"
+            }
+        };
+    }
+
+    [Fact]
+    public void Run_HappyPath()
+    {
+        // Arrange
+        var sum = new SumRuleAction
+        {
+            SourceVocabularyPrefixFieldValue = "test.amount", TargetVocabularyKeyFieldValue = "test.amount.dq.sum"
+        };
+        var entityMetadataPart = GetEntityMetadataPart();
+        // Act
+        var ruleActionResult = sum.Run(
+            _context,
+            entityMetadataPart,
+            false);
+        // Assert
+        Assert.True(ruleActionResult.IsSuccess);
+        Assert.Equal("4.14", entityMetadataPart.Properties["test.amount.dq.sum"]);
+    }
+
+    [Fact]
+    public void Run_IsPreview()
+    {
+        // Arrange
+        var sum = new SumRuleAction
+        {
+            SourceVocabularyPrefixFieldValue = "test.amount", TargetVocabularyKeyFieldValue = "test.amount.dq.sum"
+        };
+        var entityMetadataPart = new EntityMetadataPart();
+        // Act
+        var ruleActionResult = sum.Run(
+            _context,
+            entityMetadataPart,
+            true);
+        // Assert
+        Assert.True(ruleActionResult.IsSuccess);
+        Assert.True(entityMetadataPart.Properties.ContainsKey("test.amount.dq.sum"));
+    }
+}
